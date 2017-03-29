59f8889c3b6a9c549fc82564679db9eff72a76ff
rajwinder-brightpath <rajwinder@brightpath.consulting>
2017-03-29 19:02:46 +0530

    new

---
+++ TestBot/Controllers/CSHttpClientSample.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Net.Http;
+
+
+namespace TestBot.Controllers
+{
+    public class CSHttpClientSample
+    {
+        static void Main()
+        {
+            MakeRequest();
+            Console.WriteLine("Hit ENTER to exit...");
+            Console.ReadLine();
+        }
+
+        static async void MakeRequest()
+        {
+            var client = new HttpClient();
+            var queryString = HttpUtility.ParseQueryString(string.Empty);
+
+            // Request headers
+            client.DefaultRequestHeaders.Add("Ocp-Apim-Subscription-Key", "{16c5a1e70abf426aa3b3f97604b292da}");
+
+            var uri = "https://westus.api.cognitive.microsoft.com/qnamaker/v2.0/knowledgebases/create?" + queryString;
+
+            HttpResponseMessage response;
+
+            // Request body
+            byte[] byteData = Encoding.UTF8.GetBytes("{body}");
+
+            using (var content = new ByteArrayContent(byteData))
+            {
+                content.Headers.ContentType = new MediaTypeHeaderValue("< your content type, i.e. application/json >");
+                response = await client.PostAsync(uri, content);
+            }
+
+        }
+    }
+}
+
+
+//namespace CSHttpClientSample
+//{
+//    static class Program
+//    {
+//        static void Main()
+//        {
+//            MakeRequest();
+//            Console.WriteLine("Hit ENTER to exit...");
+//            Console.ReadLine();
+//        }
+
+//        static async void MakeRequest()
+//        {
+//            var client = new HttpClient();
+//            var queryString = HttpUtility.ParseQueryString(string.Empty);
+
+//            // Request headers
+//            client.DefaultRequestHeaders.Add("Ocp-Apim-Subscription-Key", "{subscription key}");
+
+//            var uri = "https://westus.api.cognitive.microsoft.com/qnamaker/v2.0/knowledgebases/create?" + queryString;
+
+//            HttpResponseMessage response;
+
+//            // Request body
+//            byte[] byteData = Encoding.UTF8.GetBytes("{body}");
+
+//            using (var content = new ByteArrayContent(byteData))
+//            {
+//                content.Headers.ContentType = new MediaTypeHeaderValue("< your content type, i.e. application/json >");
+//                response = await client.PostAsync(uri, content);
+//            }
+
+//        }
+//    }
+//}
--- TestBot/Controllers/MessagesController.cs
+++ TestBot/Controllers/MessagesController.cs
@@ -7,12 +7,112 @@ using System.Web.Http;
 using System.Web.Http.Description;
 using Microsoft.Bot.Connector;
 using Newtonsoft.Json;
+using System.Text;
+using System.Web;
+using System.Net.Http.Headers;
+using System.Collections.Generic;
+using System.Web.Script.Serialization;
+using System.IO;
 
 namespace TestBot
 {
     [BotAuthentication]
     public class MessagesController : ApiController
     {
+        //static void Main()
+        //{
+        //    MakeRequest();
+        //    Console.WriteLine("Hit ENTER to exit...");
+        //    Console.ReadLine();
+        //}
+
+        static async void MakeRequest()
+        {
+         //   String result = MakePublishRequest();
+            var client = new HttpClient();
+
+            var queryString = HttpUtility.ParseQueryString(string.Empty);
+
+            // Request headers
+            client.DefaultRequestHeaders.Add("Ocp-Apim-Subscription-Key", "16c5a1e70abf426aa3b3f97604b292da");
+
+            var uri = "https://westus.api.cognitive.microsoft.com/qnamaker/v2.0/knowledgebases/create";// + queryString;
+
+            HttpResponseMessage response;
+            //string Body = @'"{"name" : "My Knowledgebase", "urls": [ "http://www.seattle.gov/hala/faq" ]}"';
+            //var serialized = JsonConvert.SerializeObject(Body);
+            // Request body
+            //string json = "{ \"name\": \"sendKnowledgebase\", " +
+            //    " \"qnaPairs\": [" +
+            //   "  \"answer\": \"ticket online, via your mobile device\"," +
+            //" \"question\": \"How do I buy a timed  \" " +
+            //   "]" +
+            //        ",  \"urls\":  \"http://www.seattle.gov/hala/faq\"}";
+
+
+           // string testjson = "{\"name\": \"sendKnowledgebase\"}";
+            //var dict = new Dictionary<string, string>
+            //{
+            //     {"name", "Knowledgebase"},
+            //    {"urls","http://www.seattle.gov/hala/faq"}
+
+            //};
+
+            //var serializer = new JavaScriptSerializer();
+            //serializer.Serialize(dict);
+            byte[] byteData = Encoding.UTF8.GetBytes(MakeQuestionBank());
+
+            using (var content = new ByteArrayContent(byteData))
+            {
+                content.Headers.ContentType = new MediaTypeHeaderValue("application/json");
+                response = await client.PostAsync(uri, content);
+            }
+
+        }
+        //public static async Task<string> MakePublishRequest()
+        //{
+        //    string ret = string.Empty;
+        //    StreamWriter requestWriter;
+        //    var webRequest = System.Net.WebRequest.Create("https://westus.api.cognitive.microsoft.com/qnamaker/v2.0/knowledgebases/create") as HttpWebRequest;
+        //    if (webRequest != null)
+        //    {
+        //        webRequest.Method = "POST";
+        //        webRequest.ServicePoint.Expect100Continue = false;
+        //       // webRequest.Timeout = 20000;
+        //        webRequest.Headers.Add("Ocp-Apim-Subscription-Key", "16c5a1e70abf426aa3b3f97604b292da");
+        //        webRequest.ContentType = "application/json";
+        //        //POST the data.
+        //        using (requestWriter = new StreamWriter(webRequest.GetRequestStream()))
+        //        {
+        //           // string testjson = "{\"name\": \"sendKnowledgebase\"}";
+        //            requestWriter.Write(MakeQuestionBank());
+        //        }
+        //    }
+
+        //    HttpWebResponse resp = (HttpWebResponse) await webRequest.GetResponseAsync();
+          
+        //    Stream resStream = resp.GetResponseStream();
+        //    StreamReader reader = new StreamReader(resStream);
+        //    ret = reader.ReadToEnd();
+        //    return ret;
+        //}
+        //public static async Task<string> MakePublishRequest()
+        //{
+        //    string url = "http://www.seattle.gov/hala/faq";
+        //    var client = new HttpClient();
+
+        //    HttpResponseMessage response;
+
+        //    HttpContent httpContent = new StringContent("hi", Encoding.UTF8, "application/json");
+        //    httpContent.Headers.Add("Ocp-Apim-Subscription-Key", "16c5a1e70abf426aa3b3f97604b292da");
+        //    Console.WriteLine("Going to call: " + url);
+        //    response = await client.PutAsync(new Uri(url), httpContent);
+        //    response.StatusCode.ToString();
+        //    Console.WriteLine("QNAMaker MakePublishRequest statuscode: " + response.StatusCode.ToString());
+        //    Console.WriteLine("QNAMaker MakePublishRequest response: " + response.ToString());
+        //    return response.StatusCode.ToString();
+
+        //}
 
         public static async Task<string> RequestAsync<T>(string input)
         {
@@ -23,8 +123,8 @@ namespace TestBot
             string responseString = string.Empty;
 
             string query = input; //User Query
-            string knowledgebaseId = "6375e403-e4a2-4f5e-88ee-1309f703f38f"; // Use knowledge base id created.
-            string qnamakerSubscriptionKey = "388f6dd5a1404662bd763f638b1c54cf"; //Use subscription key assigned to you.
+            string knowledgebaseId = "6446ae5d-2685-462a-8163-03bb27dd650c"; // Use knowledge base id created.
+            string qnamakerSubscriptionKey = "16c5a1e70abf426aa3b3f97604b292da"; //Use subscription key assigned to you.
 
             //Build the URI
             Uri qnamakerUriBase = new Uri("https://westus.api.cognitive.microsoft.com/qnamaker/v1.0");
@@ -41,7 +141,7 @@ namespace TestBot
 
                 //Add the subscription key header
                 client.Headers.Add("Ocp-Apim-Subscription-Key", qnamakerSubscriptionKey);
-                client.Headers.Add("Content-Type", "application/json");
+                client.Headers.Add("Content-Type", "text/json");
                 responseString = client.UploadString(builder.Uri, postBody);
             }
             return responseString;
@@ -53,9 +153,12 @@ namespace TestBot
         /// </summary>
         public async Task<HttpResponseMessage> Post([FromBody]Activity activity)
         {
+          
+          //  String result = await MakePublishRequest();
             if (activity.Type == ActivityTypes.Message)
             {
-               string getanswer=  await RequestAsync<string>(activity.Text);
+                MakeRequest();
+                string getanswer=  await RequestAsync<string>(activity.Text);
                 ConnectorClient connector = new ConnectorClient(new Uri(activity.ServiceUrl));
                 // calculate something for us to return
                 int length = (activity.Text ?? string.Empty).Length;
@@ -101,5 +204,42 @@ namespace TestBot
 
             return null;
         }
+
+        public static string MakeQuestionBank()
+        {
+            QnaPair qna = new QnaPair() { question = "What is your name ?", answer = "My name is Raj" };
+
+            AddToKB questionbank = new AddToKB() { name = "Kbtest8883",qnaPairs=new List<QnaPair>(),urls=new List<string>() };
+            questionbank.qnaPairs.Add(qna);
+            questionbank.urls.Add("http://www.seattle.gov/hala/faq");
+
+
+            var serializer = new JavaScriptSerializer();
+            return serializer.Serialize(questionbank);
+
+
+        }
+
+
+    }
+    public class QnaPair
+    {
+
+        [JsonProperty("answer")]
+        public string answer { get; set; }
+
+        [JsonProperty("question")]
+        public string question { get; set; }
+    }
+
+    public class AddToKB
+    {
+        [JsonProperty("name")]
+        public string name { get; set; }
+        [JsonProperty("qnaPairs")]
+        public IList<QnaPair> qnaPairs { get; set; }
+
+        [JsonProperty("urls")]
+        public IList<string> urls { get; set; }
     }
 }
